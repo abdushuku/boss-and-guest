fb1acfb36e1ff529b143a227e88bbbf82480d8d6
Abdushukur <abduhakimov@gmail.com>
2022-10-06 12:08:54 +0500

    boss and guest

---
+++ media.js
@@ -0,0 +1,6 @@
+var username = prompt('ismingizni kiriting')
+if (username == 'abdushukur' || username == ' Abdushukur' || username == 'ABDUSHUKUR'){
+    alert('Hello Boss')
+}else{
+    alert('Hello guest')
+}
